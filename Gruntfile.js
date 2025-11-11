9e564c750155017c31030348ff002b7abd27823c
Bas Brands <bas@sonsbeekmedia.nl>
2025-11-11 14:20:12 +0100

    Better way of compiling scss

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/* eslint-env node */
+/* jshint node: true */
+/* jshint esversion: 6 */
+
+module.exports = grunt => {
+    const path = require('path');
+    const originalCwd = process.cwd();
+    const moodleRoot = path.resolve(__dirname, '../../');
+
+    try {
+        // Se déplacer vers la racine de Moodle
+        process.chdir(moodleRoot);
+
+        // Charger le Gruntfile racine depuis le contexte de la racine
+        const rootGruntfile = path.join(moodleRoot, 'Gruntfile.js');
+        if (grunt.file.exists(rootGruntfile)) {
+            require(rootGruntfile)(grunt);
+        }
+
+        // Étendre la configuration existante avec des chemins absolus
+        grunt.config.merge({
+            sass: {
+                projetvet: {
+                    files: {
+                        [path.join(originalCwd, "styles.css")]: path.join(originalCwd, "scss/styles.scss")
+                    },
+                    options: {
+                        implementation: require('sass'),
+                        includePaths: [path.join(originalCwd, "scss/")],
+                        outputStyle: 'expanded',
+                        indentWidth: 4
+                    }
+                }
+            }
+        });
+
+        // Créer une tâche qui utilise directement la configuration sass
+        grunt.registerTask('build:projetvet', ['sass:projetvet']);
+
+    } catch (error) {
+        grunt.log.error('Erreur lors du chargement du Gruntfile racine:', error.message);
+
+        // Revenir au répertoire original pour la configuration de base
+        process.chdir(originalCwd);
+
+        grunt.loadNpmTasks('grunt-sass');
+        grunt.initConfig({
+            sass: {
+                projetvet: {
+                    files: {
+                        "styles.css": "scss/styles.scss"
+                    },
+                    options: {
+                        implementation: require('sass'),
+                        includePaths: ["scss/"],
+                        outputStyle: 'expanded',
+                        indentWidth: 4
+                    }
+                }
+            }
+        });
+
+        grunt.registerTask('default', ['sass:projetvet']);
+    }
+};
